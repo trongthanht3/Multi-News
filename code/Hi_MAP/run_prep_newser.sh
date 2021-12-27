16121a0eca04fd515ab8eabf591e826cb6ebe15b
trongthanht3 <trongthanht3@gmail.com>
2021-12-27 23:28:18 +0700

    add txt

--- code/Hi_MAP/run_prep_newser.sh
+++ code/Hi_MAP/run_prep_newser.sh
@@ -1,7 +1,7 @@
-python preprocess.py -train_src ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated \
-                     -train_tgt ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated \
-                     -valid_src ./data/val.txt.src.tokenized.fixed.cleaned.final.truncated \
-                     -valid_tgt ./data/val.txt.tgt.tokenized.fixed.cleaned.final.truncated \
+python preprocess.py -train_src ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated.txt \
+                     -train_tgt ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated.txt \
+                     -valid_src ./data/val.txt.src.tokenized.fixed.cleaned.final.truncated.txt \
+                     -valid_tgt ./data/val.txt.tgt.tokenized.fixed.cleaned.final.truncated.txt \
                      -save_data ./newser_sent_500/newser_sents \
                      -src_seq_length 10000 \
                      -tgt_seq_length 10000 \
