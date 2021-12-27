5a578ff28fd2b8612d92c3215b06b1bcdfcec7b2
trongthanht3 <trongthanht3@gmail.com>
2021-12-27 23:24:04 +0700

    e

--- code/Hi_MAP/run_prep_newser.sh
+++ code/Hi_MAP/run_prep_newser.sh
@@ -1,7 +1,7 @@
 python preprocess.py -train_src ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated \
                      -train_tgt ./data/train.txt.src.tokenized.fixed.cleaned.final.truncated \
-                     -valid_src ./data/val.txt.src.tokenizd.fixed.cleaned.final.truncated \
-                     -valid_tgt ./data/val.txt.tgt.tokenizd.fixed.cleaned.final.truncated \
+                     -valid_src ./data/val.txt.src.tokenized.fixed.cleaned.final.truncated \
+                     -valid_tgt ./data/val.txt.tgt.tokenized.fixed.cleaned.final.truncated \
                      -save_data ./newser_sent_500/newser_sents \
                      -src_seq_length 10000 \
                      -tgt_seq_length 10000 \
